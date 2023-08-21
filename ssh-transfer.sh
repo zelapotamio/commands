9fcf0ec20e8c0b27461cc2125be096b4478e40a0
zelapotamio <83243656+zelapotamio@users.noreply.github.com>
2023-08-21 01:18:01 -0300

    Update ssh-transfer.sh

--- ssh-transfer.sh
+++ ssh-transfer.sh
@@ -3,7 +3,6 @@
 # Check if running as root
 if [ "$EUID" -ne 0 ]; then
   echo "Este comando deve ser executado como administrador (ou root)."
-  exit 1
 fi
 
 # Array of commands to execute
@@ -17,16 +16,15 @@ commands=(
 read -s -p "Digite sua senha do deck (Eh normal a tela ficar preta): " sudo_password
 echo
 
-# Loop through commands and execute them
-for cmd in "${commands[@]}"; do
-  echo "Executing: $cmd"
-  echo "$sudo_password" | sudo -S $cmd
-done
-
 # Extract IP address from the third command's output
 ip_address=$(ip addr | grep inet | grep wlan0 | awk '{print $2}' | sed 's/\/.*//')
-
-echo "SSH habilitado. Agora abra o  WinSCP no seu PC Windows e insira os seguintes dados:"
-echo "IP: $ip_address"
-echo "User: deck"
-echo "Password: Sua senha do deck (vc tem que saber, fiote)"
+echo "--------------------------------------------------------"
+echo "SSH habilitado."
+echo "--------------------------------------------------------"
+echo "Agora abra o  WinSCP no seu PC Windows"
+echo "E insira os seguintes dados:"
+echo "--------------------------------------------------------"
+echo "| IP: $ip_address"
+echo "| User: deck"
+echo "| Password: Sua senha do deck (vc tem que saber, fiote)"
+echo "--------------------------------------------------------"
